91ac7a1b41cf5b14ba1de72a58fec56966a96cc4
KelvenVS <kelven.serejo@gmail.com>
2023-01-24 20:33:40 -0300

    algorithms

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const costBooks = [25, 15, 30, 50, 45, 20];
+const books = require("./listBook.json");
+//console.log(books);
 
 let cheaper = 0;
-
-for (let current = 0; current < costBooks.length; current++) {
-  if (costBooks[current] < costBooks[cheaper]) {
+for (let current = 0; current < books.length; current++) {
+  if (books[current].cost < books[cheaper].cost) {
     cheaper = current;
   }
 }
 
-console.log(`O livro mais barato custa ${costBooks[cheaper]}`);
+//console.log(books[1].title);
+console.log(
+  `O livro mais barato é o ${books[cheaper].title} e custa ${books[cheaper].cost} reais`
+);
