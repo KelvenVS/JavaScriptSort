5655249876e1abc8bd92f2ed5b61bce4a367eefa
KelvenVS <kelven.serejo@gmail.com>
2023-01-24 21:21:00 -0300

    function lower cost

---
+++ lowercost.js
@@ -0,0 +1,29 @@
+const arrProducts = require("./listBook.json");
+//console.log(books);
+
+//                 any array
+function lowerCost(arrProducts, startPosition) {
+  let cheaper = startPosition;
+  for (let current = startPosition; current < arrProducts.length; current++) {
+    if (arrProducts[current].cost < arrProducts[cheaper].cost) {
+      cheaper = current;
+    }
+  }
+  return cheaper;
+}
+
+module.exports = lowerCost;
+
+
+// let cheaper = 0;
+// for (let current = 0; current < books.length; current++) {
+//   if (books[current].cost < books[cheaper].cost) {
+//     cheaper = current;
+//   }
+// }
+// //console.log(books[1].title);
+// console.log(
+//   `O livro mais barato é o ${books[cheaper].title} e custa ${books[cheaper].cost} reais`
+// );
+
+
