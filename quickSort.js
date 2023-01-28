9dcb1fd88a91159108456829abbcf7d33b4105ac
KelvenVS <kelven.serejo@gmail.com>
2023-01-28 17:02:26 -0300

    add quickSort

--- quickSort.js
+++ quickSort.js
@@ -1,49 +1,16 @@
-const books = require("./books.json");
+const books = require('./books.json');
 
-function findSmall(pivot, array) {
-  let small = 0;
-
-  for (let current = 0; current < array.length; current++) {
-    let prodCurrent = array[current];
-
-    if (prodCurrent.cost < pivot.cost) {
-      small++;
-    }
-  }
-
-  swapSpot(array, array.indexOf(pivot), small);
-  return array;
-}
-
-function swapSpot(array, of, to) {
-  //save
-  const elem1 = array[of];
-  const elem2 = array[to];
-
-  //swap
-  array[to] = elem1;
-  array[of] = elem2;
-}
-
-function centerOfArray(array) {
-  // find pivot in array
-  let pivot = array[Math.floor(array.length / 2)];
-  console.log(pivot);
-  //call function
-  findSmall(pivot, array);
-
-  let smallValues = 0;
-  for (let analysing = 0; analysing < array.length; analysing++) {
-    let current = array[analysing];
-
-    if (current.cost <= pivot.cost && current !== pivot) {
-      swapSpot(array, analysing, smallValues);
-      smallValues++;
-    }
+function quickSort(array,left,right) {
+  if(array.length > 1) {
+    let varTemp = partition(array,left,right);
   }
 
   return array;
 }
 
-// console.log(findSmall(books[2], books));
-console.log(centerOfArray(books));
+function partition(array,left,right) {
+  let pivot = array[Math.floor((left+right)/2)];
+  return pivot;
+}
+
+console.log(quickSort(books,0,books.length - 1))
