10cb58c98c7d78fbea7c35302bb4906f089b9de2
KelvenVS <kelven.serejo@gmail.com>
2023-01-28 15:34:58 -0300

    add async function and timer log

--- sleepfuncton.js
+++ sleepfuncton.js
@@ -6,10 +6,10 @@ async function incrementNumber(number){
     console.log(number)
     await sleep(2000)
 
-    if (number == 10){
+    if (number === 10){
         return 'expired timer'
     }
-    return number++; 
+    return incrementNumber(number + 1); 
 }
 
 incrementNumber(1);
