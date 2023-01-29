ecf458777bd1966ff081a86a15b48c3444ad19d2
KelvenVS <kelven.serejo@gmail.com>
2023-01-29 12:58:41 -0300

    quickSort Function

--- quickSort.js
+++ quickSort.js
@@ -1,16 +1,40 @@
-const books = require('./books.json');
+const books = require("./books.json");
+const swapSpot = require("./findSmall"); //swapSpot funciton
 
-function quickSort(array,left,right) {
-  if(array.length > 1) {
-    let varTemp = partition(array,left,right);
+function quickSort(array, left, right) {
+  if (array.length > 1) {
+    let currentIndex = partition(array, left, right);
+    if (left < currentIndex - 1) {
+      quickSort(array, left, currentIndex - 1);
+    }
+    if (currentIndex < right) {
+      quickSort(array, currentIndex, right);
+    }
   }
-
   return array;
 }
 
-function partition(array,left,right) {
-  let pivot = array[Math.floor((left+right)/2)];
-  return pivot;
+function partition(array, left, right) {
+  console.log(`array`, array);
+  console.log(`left, right` , left, right);
+  let pivot = array[Math.floor((left + right) / 2)];
+  let currentLeft = left; //0
+  let currentRight = right; //books.length - 1
+
+  while (currentLeft <= currentRight) {
+    while (array[currentLeft].cost < pivot.cost) {
+      currentLeft++;
+    }
+    while (array[currentRight].cost > pivot.cost) {
+      currentRight--;
+    }
+    if (currentLeft <= currentRight) {
+      swapSpot(array, currentLeft, currentRight);
+      currentLeft++;
+      currentRight--;
+    }
+  }
+  return currentLeft;
 }
 
-console.log(quickSort(books,0,books.length - 1))
+console.log(quickSort(books, 0, books.length - 1));
